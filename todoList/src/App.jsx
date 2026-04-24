193c434ea404f1a37ff22d40e51678512e8889ac
abhishek-webdev <silothabhishek1303@gmail.com>
2026-04-24 12:39:30 +0530

    Added edit feature in Todo List

--- todoList/src/App.jsx
+++ todoList/src/App.jsx
@@ -4,11 +4,8 @@ import './App.css'
 
 function App() {
   return (
-    // min-h-screen makes the background cover the whole page
-    // bg-slate-50 gives it a clean, soft look
     <div className="min-h-screen bg-slate-50 py-10 px-4">
       
-      {/* Container to keep everything centered and responsive */}
       <div className="max-w-md mx-auto bg-white shadow-xl rounded-2xl overflow-hidden">
         
         <div className="p-6 bg-green-600">
